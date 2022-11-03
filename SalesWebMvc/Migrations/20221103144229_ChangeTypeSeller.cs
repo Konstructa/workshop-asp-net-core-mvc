ac1104c9f97ef8406d70ccd1dc977914b26a6f49
Milena Elen <elenprojects7@gmail.com>
2022-11-03 12:43:41 -0300

    Create Seed

---
+++ SalesWebMvc/Migrations/20221103144229_ChangeTypeSeller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class ChangeTypeSeller : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "BaseSalary",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BaseSalary",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
